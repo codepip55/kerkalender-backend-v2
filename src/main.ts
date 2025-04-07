7571d5c2e359b39f41fcdff0ddd1a6333d71ee27
Pip <92968130+codepip55@users.noreply.github.com>
2025-04-07 13:25:54 +0200

    Setup cors, cookies and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from "@nestjs/common";
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set cookie options
+  const configService = app.get(ConfigService);
+  const cookieSecret = configService.get<string>('COOKIE_SECRET');
+  app.use(cookieParser(cookieSecret));
+
+  // Enable CORS
+  app.enableCors({
+    origin: [/pepijncolenbrander\.com$/, /localhost(:\d+)?$/],
+    credentials: true
+  });
+
+  // Configure validation pipes
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Enable helmet for security
+  // @ts-expect-error - helmet is not typed
+  app.use(helmet());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
